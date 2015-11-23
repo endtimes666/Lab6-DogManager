f1028b8143dd2b75fd0da35fb5cb5d57813867f1
eyefloater <gobifish@gmail.com>
2015-11-23 15:28:49 -0600

    .

---
+++ src/com/gaby/Main.java
@@ -0,0 +1,25 @@
+/*You have started a dog walking business and you’d like a program to keep track of the dogs you need to walk.
+Write a Dog class. A Dog object should store a dog’s name, the owner’s address, and some means of storing 
+how many times a week, and on what days the dog should be walked.
+One way of doing this is to have a 7-element array representing the days of the week. 
+Each element would store the number of times the dog should be walked that day. 
+So, if we decide the first day of the week is Monday, and a particular dog needs to be walked 
+once on Mon, Tues, Thurs and Friday, twice on Wednesday, and not at the weekend, 
+the array would store 1, 1, 2, 1, 1, 0, 0.
+OR perhaps you’d like to store this data in a different way – it’s up to you.
+
+Test your Dog class by creating an application called DogManager to create Dog objects. 
+Create some sample Dog objects to represent some fictitious clients.
+Add code to DogManager to print out your dog walking schedule. So, it might display
+Monday: Walk Molly 1 time. Address: 100 Main Street
+Walk Nina 2 times. Address: 1234 Broad Street
+Tuesday: Walk Duke 3 times. Address 300 2nd Street
+….
+Add methods to Dog to help you print out this data. 
+Can you add code that prints a warning message if you have more than 6 dogs to walk in one day?*/
+
+package com.gaby;
+
+public class Main {
+
+}
